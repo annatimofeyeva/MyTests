a1b16499fc556ea9c8c1f539a221b40591e26947
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-09-14 13:53:33 -0700

    add Intent and OnClickListener to navigte to new activity

--- app/src/main/java/com/epicodus/annatimofeeva/mytests/MainActivity.java
+++ app/src/main/java/com/epicodus/annatimofeeva/mytests/MainActivity.java
@@ -1,19 +1,33 @@
 package com.epicodus.annatimofeeva.mytests;
 
+import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button goToSecondButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_LONG).show();
+       goToSecondButton = (Button)findViewById(R.id.showButtonID);
+        goToSecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //create new Intent Object
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 
